e68f3a34ac2ee137623e57ab309868b57f7568bd
Murtadha Almoamen <murtadhaalmoamen8@gmail.com>
2025-02-03 14:48:20 +0300

    complete the lab

---
+++ src/components/BurgerStack/BurgerStack.jsx
@@ -0,0 +1,18 @@
+// src/components/BurgerStack/BurgerStack.jsx
+
+const BurgerStack = (props) => {
+    return (
+        <>
+            <ul>
+                {props.stack.map((ingredient, index) => (
+                    <li key={index} style={{ backgroundColor: ingredient.color }}>
+                        {ingredient.name}
+                        <button onClick={() => {props.removeFromBurger(index)}}>X</button>
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
+};
+
+export default BurgerStack;
